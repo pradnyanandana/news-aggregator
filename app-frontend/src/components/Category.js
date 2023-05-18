ff8416b088d0c18f17d9fb0bc04e7dccf266e6e8
Pradnyanandana Suwitra <pradnyanandana@gmail.com>
2023-05-18 11:47:51 +0800

    update category page

--- app-frontend/src/components/Category.js
+++ app-frontend/src/components/Category.js
@@ -25,7 +25,30 @@ const Category = ({ category }) => {
             </div>
           </article>
         </div>
-        <div className="second-wrapper"> </div>
+        <div className="second-wrapper">
+          {[1, 2, 3, 4, 5, 6].map((r) => (
+            <article>
+              <div>
+                <div className="img-container">
+                  <img src="/sample-image.jpg" alt="Article"></img>
+                </div>
+              </div>
+              <div>
+                <div className="container">
+                  <span className="info">
+                    <p>International New York Times</p>
+                  </span>
+                  <span className="info">
+                    <p>Anna Mercury</p>
+                    <span></span>
+                    <p>January 11th 2023</p>
+                  </span>
+                  <h5 className="title">The Latest Cat Trends: Hip or Hype?</h5>
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
       </div>
     </div>
   );
